cf3e1f7dc5522c11a98155883a2c19d667895816
llemosv <leonardo.lemos2002@hotmail.com>
2023-05-18 11:35:54 -0300

    Aula 03

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,6 @@
 import { StatusBar } from "expo-status-bar";
+import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
+import { useEffect } from "react";
 import { ImageBackground, View, Text, TouchableOpacity } from "react-native";
 import {
   useFonts,
@@ -6,25 +8,75 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-
-import blurBg from "./src/assets/bg-blur.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
-
 import { styled } from "nativewind";
+import * as SecureStore from "expo-secure-store";
+
+import blurBg from "../src/assets/bg-blur.png";
+import Stripes from "../src/assets/stripes.svg";
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
+import { api } from "../src/lib/api";
+import { useRouter } from "expo-router";
 
 const StyledStripes = styled(Stripes);
 
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/ecec59592d1c3ead2cfe",
+};
+
 export default function App() {
+  const router = useRouter();
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
 
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "ecec59592d1c3ead2cfe",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post("register", {
+      code,
+    });
+
+    const { token } = response.data;
+
+    await SecureStore.setItemAsync("token", token);
+
+    router.push("/memories");
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   "aq",
+    //   makeRedirectUri({
+    //     scheme: "nlwspacetime",
+    //   })
+    // );
+
+    if (response?.type === "success") {
+      const { code } = response.params;
+
+      handleGithubOAuthCode(code);
+    }
+  }, [response]);
+
   if (!hasLoadedFonts) {
     return null;
   }
+
   return (
     <ImageBackground
       source={blurBg}
@@ -49,6 +101,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => promptAsync()}
         >
           <Text className="text-clack font-alt text-sm uppercase">
             Cadastrar Lembrança
